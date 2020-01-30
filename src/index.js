e9d86f30d8e4aa76c3e205e128d0507ff64936e8
Fares Khiary <fares.khiary@talan.com>
2020-01-30 11:23:01 +0100

    code refactor

--- src/index.js
+++ src/index.js
@@ -1,66 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-// Todo convert class component to function component (done)
-// class Square extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             value: null
-//         };
-//     }
-//
-//     render() {
-//         return (
-//             <button
-//                 className="square"
-//                 onClick={_ => this.props.onClick()}>
-//                 {this.props.value}
-//             </button>
-//         );
-//     }
-// }
-
-function Square(props) {
-    return (
-        <button className="square"
-                onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
-}
-
-class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={this.props.squares[i]}
-                       onClick={_ => this.props.onClick(i)}
-        />;
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {/*<Square value={0} />*/}
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
+import Board from "./board";
 
 class Game extends React.Component {
     constructor(props) {
@@ -136,8 +77,6 @@ class Game extends React.Component {
     }
 
 }
-
-// ========================================
 
 ReactDOM.render(
     <Game/>,
