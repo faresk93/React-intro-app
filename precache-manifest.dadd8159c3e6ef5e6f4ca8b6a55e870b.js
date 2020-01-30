4923a0bb9b5a0d40ef4e803fd25a677cd300651c
Fares Khiary <fares.khiary@talan.com>
2020-01-30 10:47:36 +0100

    Updates

---
+++ precache-manifest.dadd8159c3e6ef5e6f4ca8b6a55e870b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0c365e24d89e0c418316d6020f1860",
+    "url": "/React-intro-app/index.html"
+  },
+  {
+    "revision": "b0b24740525c1c6e813f",
+    "url": "/React-intro-app/static/css/main.c7ab3b04.chunk.css"
+  },
+  {
+    "revision": "526e6fae8799a2a2b5af",
+    "url": "/React-intro-app/static/js/2.fae34771.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-intro-app/static/js/2.fae34771.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0b24740525c1c6e813f",
+    "url": "/React-intro-app/static/js/main.91996a00.chunk.js"
+  },
+  {
+    "revision": "fcccafdfe972d9a408d7",
+    "url": "/React-intro-app/static/js/runtime-main.ef61437d.js"
+  }
+]);
